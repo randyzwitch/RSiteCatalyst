b070bfb7b9519b19c6a0b012b7ce2a5570cbdd42
Willem Paling <willem.paling@gmail.com>
2014-02-20 23:01:48 +1100

    Updated GetElements and GetMetrics to loop over a list of ReportSuite IDs

--- R/GetElements.R
+++ R/GetElements.R
@@ -3,7 +3,7 @@
 #' Gets valid elements for a report suite for the current user. 
 #' This list is restricted by optionally specified existing elements, existing metrics and date granularity.
 #'
-#' @param reportsuite.id report suite id
+#' @param reportsuite.ids single report suite id, or character vector of report suite ids
 #' @param metrics list of existing metrics you want to use in combination with an additional metric
 #' @param elements list of existing elements you want to use in combination with an additional metric
 #' @param date.granularity granularity that you want to combine with an additional metric
@@ -18,24 +18,34 @@
 #' \dontrun{
 #' elements.valid <- GetElements("your_report_suite",metrics=c('visitors','pageviews'),
 #' elements=c('page','geoCountry'),date.granularity='day')
+#' 
+#' elements <- GetElements(c("your_prod_report_suite","your_dev_report_suite"))
 #' }
 
-GetElements <- function(reportsuite.id, metrics=c(), elements=c(), date.granularity='') {
+GetElements <- function(reportsuite.ids, metrics=c(), elements=c(), date.granularity='') {
   
-  request.body <- c()
-  request.body$reportSuiteID <- jsonlite:::as.scalar(reportsuite.id)
+  valid.elements <- data.frame()
+  for(reportsuite.id in reportsuite.ids) {
+    request.body <- c()
+    request.body$reportSuiteID <- jsonlite:::as.scalar(reportsuite.id)
 
-  if(length(metrics)>0) { 
-    request.body$reportDescription$existingElements <- metrics
+    if(length(metrics)>0) { 
+      request.body$reportDescription$existingElements <- metrics
+    }
+    if(length(elements)>0) { 
+      request.body$reportDescription$existingElements <- elements
+    }
+    if(nchar(date.granularity)>0) { 
+      request.body$reportDescription$dateGranularity <- jsonlite:::as.scalar(date.granularity) 
+    }
+    working.elements <- ApiRequest(body=toJSON(request.body),func.name="Report.GetElements")
+    working.elements$rsid <- reportsuite.id
+    if(length(valid.elements)){
+      valid.elements <- rbind(valid.elements,working.elements)
+    } else {
+      valid.elements <- working.elements
+    }
   }
-  if(length(elements)>0) { 
-    request.body$reportDescription$existingElements <- elements
-  }
-  if(nchar(date.granularity)>0) { 
-    request.body$reportDescription$dateGranularity <- jsonlite:::as.scalar(date.granularity) 
-  }
-
-  valid.elements <- ApiRequest(body=toJSON(request.body),func.name="Report.GetElements")
 
   return(valid.elements)
 
--- R/GetMetrics.R
+++ R/GetMetrics.R
@@ -2,7 +2,7 @@
 #'
 #' Gets valid metrics for current user, valid with optionally specified existing metrics, elements and date granularity
 #'
-#' @param reportsuite.id report suite id
+#' @param reportsuite.ids single report suite id, or character vector of report suite ids
 #' @param metrics list of existing metrics you want to use in combination with an additional metric
 #' @param elements list of existing elements you want to use in combination with an additional metric
 #' @param date.granularity granularity that you want to combine with an additional metric
@@ -19,22 +19,34 @@
 #' elements=c('page','geoCountry'),date.granularity='day')
 #' }
 
-GetMetrics <- function(reportsuite.id, metrics=c(), elements=c(), date.granularity='') {
+GetMetrics <- function(reportsuite.ids, metrics=c(), elements=c(), date.granularity='') {
   
-  request.body <- c()
-  request.body$reportSuiteID <- jsonlite:::as.scalar(reportsuite.id)
+  valid.metrics <- data.frame()
+  
+  for(reportsuite.id in reportsuite.ids) {
 
-  if(length(metrics)>0) { 
-    request.body$reportDescription$existingMetrics <- metrics
-  }
-  if(length(elements)>0) { 
-    request.body$reportDescription$existingElements <- elements
-  }
-  if(nchar(date.granularity)>0) { 
-    request.body$reportDescription$dateGranularity <- jsonlite:::as.scalar(date.granularity) 
-  }
+    request.body <- c()
+    request.body$reportSuiteID <- jsonlite:::as.scalar(reportsuite.id)
 
-  valid.metrics <- ApiRequest(body=toJSON(request.body),func.name="Report.GetMetrics")
+    if(length(metrics)>0) { 
+      request.body$reportDescription$existingMetrics <- metrics
+    }
+    if(length(elements)>0) { 
+      request.body$reportDescription$existingElements <- elements
+    }
+    if(nchar(date.granularity)>0) { 
+      request.body$reportDescription$dateGranularity <- jsonlite:::as.scalar(date.granularity) 
+    }
+
+    working.metrics <- ApiRequest(body=toJSON(request.body),func.name="Report.GetMetrics")
+    working.metrics$rsid <- reportsuite.id
+    if(length(valid.metrics)){
+      valid.metrics <- rbind(valid.metrics,working.metrics)
+    } else {
+      valid.metrics <- working.metrics
+    }
+    
+  }
 
   return(valid.metrics)
 
