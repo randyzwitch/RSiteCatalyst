17e4d898560ce4f82249effbdee567b0955b113f
Randy Zwitch <rzwitch@gmail.com>
2014-04-20 10:23:11 -0400

    More docs changes

--- R/BuildClassificationValueSegment.R
+++ R/BuildClassificationValueSegment.R
@@ -1,21 +1,29 @@
-#' BuildClassificationValueSegment
+#' @name BuildClassificationValueSegment
 #'
-#' Helper function to build a classification value segment for use with other reports
+#' @details Helper function to build a classification value segment for use with other reports.
+#' 
 #' Multiple segments can be combined in a list.
 #' Note that search can only be applied to a breakdown classification and not an element value.
 #'
 #' @title Build a Classification Value Segment
 #' 
-#' @param element list of elements on which to vase the segment
-#' @param search.keywords list of search keyword vectorsfor each element 
+#' @param element List of elements on which to vase the segment
+#' @param search.keywords List of search keyword vectorsfor each element 
 #' (this can use ^ to pin to start, and $ to pin to end, or both to specify exact match)
-#' @param classification optional classification breakdown name for the element (defaults to the element name)
-#' @param search.type how to combine the keywords list. This defaults to 'OR' if it is not specified.
+#' @param classification (optional) Classification breakdown name for the element (defaults to the element name)
+#' @param search.type How to combine the keywords list. This defaults to 'OR' if it is not specified.
 #'
 #' @importFrom jsonlite toJSON unbox
 #'
-#' @return segment definition for use with Queue* helper functions
+#' @return Segment definition for use with Queue* functions
 #'
+#' @examples
+#' \dontrun{
+#' vistor_segment <- BuildClassificationValueSegment(element,
+#'                                                   search.keywords,
+#'                                                   classification,
+#'                                                   search.type)
+#' }
 #' @export
 
 BuildClassificationValueSegment <- function(element,search.keywords,classification='',search.type='OR') {
