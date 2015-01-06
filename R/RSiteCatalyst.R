311d07e206db002317a87ceaa41c269105604a9e
Randy Zwitch <rzwitch@gmail.com>
2015-01-06 09:21:59 -0500

    Submit to CRAN for 1.4.3

--- R/RSiteCatalyst.R
+++ R/RSiteCatalyst.R
@@ -1,7 +1,7 @@
 #' Package:  RSiteCatalyst \cr
 #' Type:     Package \cr
-#' Version:  1.4.2.20150105 \cr
-#' Date:     2015-01-05 \cr
+#' Version:  1.4.3 \cr
+#' Date:     2015-01-06 \cr
 #' License:  MIT + file LICENSE \cr
 #'
 #'
