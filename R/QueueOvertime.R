26349df00ba471047afa08d1a3426b0b228ef5d7
Gitzman <adam.i.gitzes@gmail.com>
2016-03-16 23:06:04 -0400

    Segment Stacking, need to dedupe the results

--- R/QueueOvertime.R
+++ R/QueueOvertime.R
@@ -70,7 +70,6 @@ QueueOvertime <- function(reportsuite.id, date.from, date.to, metrics,
   report.description$reportDescription$dateTo <- unbox(date.to)
   report.description$reportDescription$reportSuiteID <- unbox(reportsuite.id)
   report.description$reportDescription$dateGranularity <- unbox(date.granularity)
-  report.description$reportDescription$segment_id <- unbox(segment.id)
   report.description$reportDescription$anomalyDetection <- unbox(anomaly.detection)
   report.description$reportDescription$currentData <- unbox(data.current)
   report.description$reportDescription$expedite <- unbox(expedite)
@@ -85,6 +84,7 @@ QueueOvertime <- function(reportsuite.id, date.from, date.to, metrics,
   }
   report.description$reportDescription$metrics = data.frame(id = metrics)
 
+  report.description$reportDescription$segments <- data.frame( id = segment.id)
   report.data <- SubmitJsonQueueReport(toJSON(report.description),interval.seconds=interval.seconds,max.attempts=max.attempts,validate=validate)
 
   return(report.data)
