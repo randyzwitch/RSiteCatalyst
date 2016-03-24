de96b5003829986e46d3d01947b2da6ed499a02c
Gitzman <adam.i.gitzes@gmail.com>
2016-03-24 11:46:21 -0400

    QueueFallout Tests Fixed, actually

--- tests/testthat/test-queuefallout.R
+++ tests/testthat/test-queuefallout.R
@@ -10,7 +10,7 @@ test_that("Validate QueueFallout using legacy credentials", {
                      "2015-12-31",
                      metric="pageviews",
                      element="page",
-                     c("http://randyzwitch.com/r-amazon-ec2", "http://randyzwitch.com/julia-import-data")
+                     c("http://randyzwitch.com/r-amazon-ec2", "http://randyzwitch.com/julia-import-data"))
 
 
   #Validate returned value is a data.frame
@@ -22,7 +22,7 @@ bb <-QueueFallout("zwitchdev",
                   "2015-12-31",
                   metrics="pageviews",
                   element="page",
-                  checkpoints=c("http://randyzwitch.com/r-amazon-ec2", "http://randyzwitch.com/julia-import-data")
+                  checkpoints=c("http://randyzwitch.com/r-amazon-ec2", "http://randyzwitch.com/julia-import-data"),
                   segment.id=c("54e4e00be4b093ca5b709931"))
 
 
@@ -35,7 +35,7 @@ cc <-QueueFallout("zwitchdev",
                   "2015-12-31",
                   metrics="pageviews",
                   element="page",
-                  checkpoints=c("http://randyzwitch.com/r-amazon-ec2", "http://randyzwitch.com/julia-import-data")
+                  checkpoints=c("http://randyzwitch.com/r-amazon-ec2", "http://randyzwitch.com/julia-import-data"),
                   segment.id=c("54e4e00be4b093ca5b709931","54e62bd0e4b0619d25327e86"))
 
 #Validate returned value is a data.frame
