9f16f5ef52b7c9679691edf6b0a7247bbba14470
randyzwitch <rzwitch@gmail.com>
2016-04-03 11:21:39 -0400

    1.4.8 to CRAN

--- R/RSiteCatalyst.R
+++ R/RSiteCatalyst.R
@@ -1,6 +1,6 @@
 #' Package:  RSiteCatalyst \cr
 #' Type:     Package \cr
-#' Version:  1.4.7.20160403 \cr
+#' Version:  1.4.8 \cr
 #' Date:     2016-04-03 \cr
 #' License:  MIT + file LICENSE \cr
 #'
