ff377aa8afa1f1001a5619a4d047c5ffaf804603
randyzwitch <rzwitch@gmail.com>
2017-07-20 11:38:37 -0400

    Update package in preparation for CRAN submit

--- tests/testthat/test-queuedatawarehouse.R
+++ tests/testthat/test-queuedatawarehouse.R
@@ -23,18 +23,6 @@ test_that("Validate QueueDataWarehouse using legacy credentials", {
   #Validate returned value is numeric id
   expect_is(report.id, "numeric")
   
-  #Return answer to console
-  # qdw <- QueueDataWarehouse("zwitchdev",
-  #                                   "2016-11-01",
-  #                                   "2016-11-07",
-  #                                   c("visits", "pageviews"),
-  #                                   c("page"),
-  #                                   enqueueOnly=FALSE
-  # )
-  # 
-  # #Validate returned value is a data.frame
-  # expect_is(qdw, "data.frame")
-  
   dwresult <- QueueDataWarehouse("zwitchdev",
                                  "2014-01-01",
                                  "2017-02-02",
