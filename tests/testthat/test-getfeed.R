0a982ffab7a4bcbe4463c4e3d2c859a063bdfeb5
Randy Zwitch <rzwitch@gmail.com>
2019-11-02 20:29:45 -0400

    Clean up all but Queue methods

--- tests/testthat/test-getfeed.R
+++ tests/testthat/test-getfeed.R
@@ -1,13 +1,13 @@
 test_that("Validate GetFeed using legacy credentials", {
-  
+
   skip_on_cran()
-  
+
   #Correct [masked] credentials
   SCAuth(Sys.getenv("USER", ""), Sys.getenv("SECRET", ""))
-  
-  gf <- GetFeed("1109801")
-  
+
+  gf <- GetFeed("11346")
+
   #Validate returned value is a data.frame
   expect_is(gf, "data.frame")
-  
+
 })
