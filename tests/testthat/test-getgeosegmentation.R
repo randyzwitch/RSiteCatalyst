76594b0fd252519e80b50f881a4552272f893d89
Randy Zwitch <rzwitch@gmail.com>
2019-11-04 08:46:23 -0500

    Comment out tests to make CI faster

--- tests/testthat/test-getgeosegmentation.R
+++ tests/testthat/test-getgeosegmentation.R
@@ -1,13 +1,13 @@
-test_that("Validate GetGeoSegmentation using legacy credentials", {
-  
-  skip_on_cran()
-  
-  #Correct [masked] credentials
-  SCAuth(Sys.getenv("USER", ""), Sys.getenv("SECRET", ""))
-  
-  geoseg <- GetGeoSegmentation("zwitchdev")
-  
-  #Validate returned value is a data.frame
-  expect_is(geoseg, "data.frame")
-  
-})
+# test_that("Validate GetGeoSegmentation using legacy credentials", {
+#
+#   skip_on_cran()
+#
+#   #Correct [masked] credentials
+#   SCAuth(Sys.getenv("USER", ""), Sys.getenv("SECRET", ""))
+#
+#   geoseg <- GetGeoSegmentation("zwitchdev")
+#
+#   #Validate returned value is a data.frame
+#   expect_is(geoseg, "data.frame")
+#
+# })
